51d4df91f37bce26df0c92d7ef01fd74ca4f8e89
ShiyanovaOlga <b-oa@yandex.ru>
2023-08-28 18:39:08 +0300

    исправила на задачу 25

--- task25/Program.cs
+++ task25/Program.cs
@@ -6,23 +6,23 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-int SumNumbers(int n)
+int DegreeNumber(int a, int b)
 {
-    int sum = 0;
+    int ndegree = 1;
     // for (count <) // если известно количество итераций
-    // если неизвестно количество итераций использовать while
-    for (int i = 0; i <= n; i++)
+    // while // если неизвестно количество итераций 
+    for (int i = 1; i <= b; i++)
     {
-        sum = sum + i; //как вариант можно sum += i 
+        ndegree = ndegree * a; //как вариант можно sum += i 
         //если одно действие, можно не использовать {{ скобки}}
     }
-    return sum;
+    return ndegree;
 }
 
 Console.WriteLine("Введите число А");
 int aNumber = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите число B");
-// int bNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int bNumber = Convert.ToInt32(Console.ReadLine());
 
-int result = SumNumbers(aNumber); //лучше делать переменную на этом этапе
+int result = SumNumbers(aNumber, bNumber); //лучше делать переменную на этом этапе
 Console.WriteLine(result);
