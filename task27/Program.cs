74a3e5e185248c12018d52ac6ebbcf1f97768686
ShiyanovaOlga <b-oa@yandex.ru>
2023-08-29 13:24:04 +0300

    составила задачу, пока не работает

---
+++ task27/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, 
+// которая принимает на вход число 
+// и выдаёт (количество цифр в числе) сумму цифр в числе.
+// (452 -> 3) 452 -> 11  
+// (82 -> 2) 82 -> 10   
+// (9012 -> 4) 9012 -> 12
+
+int Quantity(int num);
+{
+    if (num < 0) then num = num * -1;
+    int count = 0;
+    while (num => 0)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Ваше натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int quantity = Quantity(number);
+Console.WriteLine($"в вашем числе" {quantity} "знаков");
