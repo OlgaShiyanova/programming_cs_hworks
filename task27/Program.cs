11ee1b53a81cbaf1c5b5c492255c3ecfd0bca44a
ShiyanovaOlga <b-oa@yandex.ru>
2023-08-29 13:46:33 +0300

    исправлено на условия задачи 27

--- task27/Program.cs
+++ task27/Program.cs
@@ -5,23 +5,40 @@
 // (82 -> 2) 82 -> 10   
 // (9012 -> 4) 9012 -> 12
 
-int Quantity(int num)
+// (задача в скобочках)
+// int Quantity(int num)
+// {
+//    if (num < 0) 
+//        num = num * -1;
+//        int count = 0;
+//    while (num > 0)
+//    {
+//        num = num / 10;
+//       count++;
+//    }
+//    return count;
+// }
+
+int SumDigites(int num)
 {
+    int sum = 0;
+    int remainder = 1;
     if (num < 0) 
         num = num * -1;
         int count = 0;
     while (num > 0)
     {
+        remainder = num % 10;
         num = num / 10;
+        sum = sum + remainder;
         count++;
     }
-    return count;
+    return sum;
 }
-
-
 
 Console.WriteLine("Ваше натуральное число");
 int number = Convert.ToInt32(Console.ReadLine());
 
-int quantity = Quantity(number);
-Console.WriteLine($"в вашем числе {quantity} знаков");
+int sumdig = SumDigites(number);
+// Console.WriteLine($"в вашем числе {quantity} знаков");
+Console.WriteLine($"сумма цифр числа {sumdig}");
