23c80f7a9610f8ea1a99cf923566382e1a6773e6
ShiyanovaOlga <b-oa@yandex.ru>
2023-08-30 19:16:54 +0300

    генерит массив случайных чисел без округления

--- task38/Program.cs
+++ task38/Program.cs
@@ -8,41 +8,41 @@
 // ([1 2 3 4 5] -> 5 8 3)
 // ([10, 11, 12, 13, 14] -> 5)
 
-double[] CreateArrayRndDouble(int size, int min, int max)
+double[] CreateArrayRndDouble(int size)
 {
     double[] arr = new double[size];
     Random rnd = new Random();
     for (int i = 0; i < arr.Length; i++)
     {
-        arr[i] = rnd.NextDouble(min, max);
+        arr[i] = rnd.NextDouble();
     }
     return arr;
 }
 
-void PrintArray(int[] arr)
+void PrintArray(double[] arr)
 {
     Console.Write("[");
     for (int i = 0; i < arr.Length; i++)
     {
         if(i < arr.Length - 1) Console.Write($"{arr[i]},");
-        else Console.Write($"{arr[i]}");
+        else Console.Write($"{arr[i]}; ");
     }
     Console.WriteLine("]");
 }
 
-int[] PairMultiplication(int[] arr)
-{
-    int size = arr.Length / 2 + arr.Length % 2;
-    int[] newArr = new int[size];
-    for (int i = 0; i < size; i++)
-    {
-        newArr[i] = arr[i] * arr[arr.Length-1-i];
-    }
-    if (arr.Length % 2 == 1) newArr[newArr.Length-1] = arr[arr.Length / 2];
-    return newArr;
-}
+// int[] PairMultiplication(int[] arr)
+// {
+//    int size = arr.Length / 2 + arr.Length % 2;
+//    int[] newArr = new int[size];
+//    for (int i = 0; i < size; i++)
+//    {
+//        newArr[i] = arr[i] * arr[arr.Length-1-i];
+//    }
+//    if (arr.Length % 2 == 1) newArr[newArr.Length-1] = arr[arr.Length / 2];
+//    return newArr;
+//}
 
-int[] array = CreateArrayRndDouble(5, 1, 100); 
+double[] array = CreateArrayRndDouble(5); 
 PrintArray(array);
 // int[] pairMultiplication = PairMultiplication(array);
 // PrintArray(pairMultiplication);
