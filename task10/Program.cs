f9c55859c9635cabfbfff9eaafb0f9879e9c90f9
ShiyanovaOlga <b-oa@yandex.ru>
2023-09-11 09:21:49 +0300

    в 3-х значном числе выводит 2ю цифру

---
+++ task10/Program.cs
@@ -0,0 +1,24 @@
+﻿// // Задача 10: Напишите программу, 
+// которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.WriteLine("Ваше трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+// ниже функция
+ int SecondDigit(int num)
+{
+   int twodigit = num / 10 % 10;
+    return (twodigit);
+}
+// используем функцию
+if (number > 99 && number < 1000)
+{
+ //   z = SecondDigit(number);
+    Console.WriteLine($"вторая цифра {SecondDigit(number)} ");
+}
+else 
+{
+    Console.WriteLine($"это НЕ трехзначное число. ");
+}
---
+++ task54/Program.cs
@@ -0,0 +1,58 @@
+﻿//Задача 54: Задайте двумерный массив. 
+// Напишите программу, которая 
+// упорядочит по убыванию элементы каждой строки 
+// двумерного массива.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+         Console.Write("| ");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 4}");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
+void ReplacementSeats(int[,] matrix)
+{
+    int firstRow = 0;
+    int lastRow = matrix.GetLength(0) - 1;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        int temp = matrix[firstRow, j];
+        matrix[firstRow, j] = matrix[lastRow, j];
+        matrix[lastRow, j] = temp;
+    }
+}
+
+int[,] array2d = CreateMatrixRndInt(3, 4, -99, 99);
+PrintMatrix(array2d);
+Console.WriteLine();
+ReplacementSeats(array2d);
+PrintMatrix(array2d);
